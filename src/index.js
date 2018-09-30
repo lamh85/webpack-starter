b80fa9fd9fa7e1fdd04f8601a5ebbb360d546a5b
Hon Lam <hon@junglescout.com>
2018-09-29 17:32:35 -0700

    Install webpack, weback dev server, babel.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import exportTest from "./exportTest"
+
+console.log("I am from index.js")
+console.log(exportTest)
+
+let element = document.querySelector("#container")
+element.innerHTML = "hello from index.js. hooray!"
