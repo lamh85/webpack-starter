44e90ab1c53031af2ab0ec09da8b6ee2fe56d9aa
Hon Lam <hon@junglescout.com>
2019-01-27 17:01:45 -0800

    Webpack config: add comments, source mapping, environment.

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
 
   // IMPORTANT: Sort keys alphabetically
 
+  // Used for live reloading.
   devServer: {
+    // How the server should find the files.
     contentBase: './dist',
     hot: true
   },
+  devtool: 'inline-source-map',
   entry: './src/index.js',
   module: {
     rules: [
@@ -16,6 +20,8 @@ module.exports = {
     ]
   },
   output: {
+    // Generated in memory, not physical file.
+    // Has options to programmatically format filename.
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
